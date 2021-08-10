b2387d20e1ea427e497ebf5a8c47fcd8f4e9448f
padawan <mason.camp@evsck12.com>
2021-08-10 12:52:47 -0500

    Hello World

---
+++ Hello.java
@@ -0,0 +1,11 @@
+// type two slashes it is a single line comment
+/*
+    This is comment blocks
+
+    print("Hello World")
+*/
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
