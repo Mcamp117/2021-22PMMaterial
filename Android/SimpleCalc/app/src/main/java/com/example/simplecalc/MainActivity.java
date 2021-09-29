4219765b1220a1bb15976933eb2e6af3a501ab9e
padawan <mason.camp@evsck12.com>
2021-09-29 14:04:49 -0500

    calc

--- Android/SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
+++ Android/SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -11,7 +11,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText number1TXT;
+    private EditText number2TXT;
     private Button addBTN;
+    private Button divBTN;
+    private Button multBTN;
     private TextView output;
 
     @Override
@@ -21,16 +24,54 @@ public class MainActivity extends AppCompatActivity {
 
         //linked the global variables to the widgets
         number1TXT= findViewById(R.id.number1);
+        number2TXT= findViewById(R.id.number2);
         addBTN=findViewById(R.id.addBTN);
+        divBTN=findViewById(R.id.divBTN);
+        multBTN=findViewById(R.id.multBTN);
         output=findViewById(R.id.output);
 
+        //set up our buttons with an algorithm
+        divBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1=Integer.valueOf(String.valueOf(number1TXT.getText()));
+                int num2=Integer.valueOf(String.valueOf(number2TXT.getText()));
+                int answer=num2/num1;
+                output.setText(String.valueOf(answer));
+            }
+        });
+        multBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num1=Integer.valueOf(String.valueOf(number1TXT.getText()));
+                int num2=Integer.valueOf(String.valueOf(number2TXT.getText()));
+                int answer=num1*num2;
+                output.setText(String.valueOf(answer));
+            }
+        });
+    }
+
+    public void test(View v) {
+        //getting the text
+        String input= String.valueOf(number1TXT.getText());
 
     }
 
-    public void test() {
-        //getting the text
-        String input= String.valueOf(number1TXT.getText());
-        output.setText(input);
+    //adding and subtracting are for hard coding the onclick
+
+                    //An object of View is required
+    public void adding(View v){
+        int num1=Integer.valueOf(String.valueOf(number1TXT.getText()));
+        int num2=Integer.valueOf(String.valueOf(number2TXT.getText()));
+        int answer=num1+num2;
+        output.setText(String.valueOf(answer));
+    }
+
+    public void subtracting(View v){
+        int num1=Integer.valueOf(String.valueOf(number1TXT.getText()));
+        int num2=Integer.valueOf(String.valueOf(number2TXT.getText()));
+        int answer=num1-num2;
+        output.setText(String.valueOf(answer));
     }
 
 }
---
+++ PLTWJava/Unit1/Assignment/CampTextBasedAdventureGame.java
@@ -0,0 +1,5 @@
+public class CampTextBasedAdventureGame {
+    public static void main(String[] args) {
+        
+    }
+}
