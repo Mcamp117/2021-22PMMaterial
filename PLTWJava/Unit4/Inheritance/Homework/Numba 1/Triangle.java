fb4ea62cce1dcb3af85610ca4e3ee6ec32bfa163
padawan <mason.camp@evsck12.com>
2022-01-14 12:53:07 -0600

    triangle

--- PLTWJava/Unit4/Inheritance/Homework/Numba 1/Triangle.java
+++ PLTWJava/Unit4/Inheritance/Homework/Numba 1/Triangle.java
@@ -1,6 +1,30 @@
-import java.lang.reflect.GenericSignatureFormatError;
-
-public class Triangle extends GeometricObject {
+public abstract class Triangle extends GeometricObject {
     double side1,side2,side3=1.0;
+    double perimeter=(side1+side2+side3);
+    double s=(perimeter/2);
+    double a=(Math.sqrt(s*(s-side1)*(s-side2)*(s-side3)));
+    double h=2*(a/side2);
+    double area=(1/2)*(side2)*(h);
     
+
+    public Triangle(){
+        
+    }
+    public Triangle(Double side1, Double side2, Double side3){
+        this.side1=side1;
+        this.side2=side2;
+        this.side3=side3;
+        
+    }
+    public double getArea(){
+        System.out.println(a);
+        return area;
+         
+    }
+    public double getPerimeter(){
+        return perimeter;
+    }
+    public String toString(){
+        return "Triangle: side1 = "+side1+"side2 = "+side2+"side3 = "+side3;
+    }
 }
---
+++ PLTWJava/Unit4/Inheritance/Homework/Numba 1/viewTriangle.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class viewTriangle{
+    public static void main(String[] args) {
+        Scanner ui = new Scanner(System.in); 
+        System.out.println("Enter value for side1");
+        double s1=ui.nextDouble();
+    System.out.println("Enter value for side2");
+        double s2=ui.nextDouble();
+    System.out.println("Enter value for side3");
+        double s3=ui.nextDouble();
+        Triangle triangle = new Triangle(s1,s2,s3);
+    System.out.println("What is the color");
+        String color = ui.nextLine();
+        triangle.setColor(color);
+    System.out.println("Is the triangle filled?(Y/N)");
+        String yn = ui.nextLine();
+        boolean filled = true;
+        if(yn.equals("Y")){filled=true;}
+        if(yn.equals("N")){filled=false;}
+        triangle.setFilled(filled);
+        System.out.println("Area : " +triangle.getArea()+
+        "\nPerimeter : "+triangle.getPerimeter()+
+        "\nColor : "+triangle.getColor()+
+        "\nFilled : "+ triangle.isFilled());
+}
